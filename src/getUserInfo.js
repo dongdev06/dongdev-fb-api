a5b5e8e3c55bb9a932b7003cf54a42088c2d1814
GK <100568006+KhangGia1810@users.noreply.github.com>
2023-05-23 17:46:50 +0700

    Update Get Infomation User By Graph

--- src/getUserInfo.js
+++ src/getUserInfo.js
@@ -29,8 +29,59 @@ function formatData(data) {
   return retObj;
 }
 
+function formatDataGraph(data) {
+  var Obj = {};
+  for (let v in data) {
+    var res = data[v];
+    if (res.error) Obj[v] = res.error;
+    else {
+      Obj[v] = {
+        name: res.name,
+        shortName: res.short_name || null,
+        is_verified: res.is_verified != false ? true : false,
+        email: res.email || null,
+        website: res.website || null,
+        follower: !!res.subscribers == true ? res.subscribers.summary.total_count : null,
+        lover: !!res.significant_other == true ? res.significant_other.name : null,
+        cover: !!res.cover == true ? res.cover.source : null,
+        first_name: res.first_name || null,
+        middle_name: res.middle_name || null,
+        last_name: res.last_name || null,
+        about: res.about || null,
+        birthday: res.birthday || null,
+        gender: res.gender || null,
+        hometown: !!res.hometown == true ? res.hometown.name : null,
+        profileUrl: res.link || null,
+        location: !!res.location == true ? res.location.name : null,
+        username: res.username || null,
+        avatar: !!res.picture == true ? res.picture.data.url : null,
+        relationship_status: !!res.relationship_status == true ? res.relationship_status : null,
+        subscribers: !!res.subscribers == true ? res.subscribers.data : null,
+        education: !!res.education == true ? res.education.map(function(v) {
+          return {
+            type: v.type,
+            school: v.school.name
+          }
+        }) : []
+      }
+    }
+  }
+  
+  return Obj;
+}
+
 module.exports = function (defaultFuncs, api, ctx) {
-  return function getUserInfo(userIDs, callback) {
+  async function getData(userIDs, cb) {
+    var form = {};
+    for (let userID of userIDs) {
+      var res = await defaultFuncs
+        .get(`https://graph.facebook.com/v1.0/${userID}?fields=name,is_verified,cover,first_name,email,about,birthday,gender,website,hometown,link,location,quotes,relationship_status,significant_other,username,subscribers.limite(0),short_name,last_name,middle_name,education,picture&access_token=${ctx.access_token}`, ctx.jar, null, ctx.globalOptions);
+      form[userID] = JSON.parse(res.body);
+    }
+    return cb(null, form);
+  }
+  
+  return function getUserInfo(userIDs, useGraph, callback) {
     var cb;
     var returnPromise = new Promise(function (resolve, reject) {
       cb = function (error, resData) {
@@ -39,27 +90,39 @@ module.exports = function (defaultFuncs, api, ctx) {
       }
     });
 
+    if (typeof useGraph == 'function') {
+      callback = useGraph;
+      useGraph = false;
+    }
     if (typeof callback == 'function') cb = callback;
     if (Array.isArray(userIDs) == false) userIDs = [userIDs];
 
-    var form = {};
-    userIDs.map(function(v, i) {
-      form["ids[" + i + "]"] = v;
-    });
-    defaultFuncs
-      .post("https://www.facebook.com/chat/user_info/", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.error) {
-          log.error("getUserInfo", resData.error);
-          return cb(resData);
-        }
-        return cb(null, formatData(resData.payload.profiles));
-      })
-      .catch(function(err) {
-        log.error("getUserInfo", err);
-        return cb(err);
+    if (useGraph) {
+      if (ctx.access_token == 'NONE') return cb('Error: cant get access_token, please let the "useGraph" feature is false');
+      getData(userIDs, function (err, res) {
+        if (err) return cb(err);
+        return cb(null, formatDataGraph(res));
       });
+    } else {
+      var form = {};
+      userIDs.map(function(v, i) {
+        form["ids[" + i + "]"] = v;
+      });
+      defaultFuncs
+        .post("https://www.facebook.com/chat/user_info/", ctx.jar, form)
+        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+        .then(function(resData) {
+          if (resData.error) {
+            log.error("getUserInfo", resData.error);
+            return cb(resData.error);
+          }
+          return cb(null, formatData(resData.payload.profiles));
+        })
+        .catch(function(err) {
+          log.error("getUserInfo", err);
+          return cb(err);
+        });
+    }
 
     return returnPromise;
   };
