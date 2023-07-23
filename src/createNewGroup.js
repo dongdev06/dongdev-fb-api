b6f5af1c997b52feb624a85a74a8ded79bb98905
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-07-23 11:56:31 +0700

    Update Set Group Picture

--- src/createNewGroup.js
+++ src/createNewGroup.js
@@ -3,84 +3,103 @@
 var utils = require("../utils");
 var log = require("npmlog");
 
-module.exports = function(defaultFuncs, api, ctx) {
-  return function createNewGroup(participantIDs, groupTitle, callback) {
-    if (utils.getType(groupTitle) == "Function") {
-      callback = groupTitle;
-      groupTitle = null;
-    }
-
-    if (utils.getType(participantIDs) !== "Array") {
-      throw { error: "createNewGroup: participantIDs should be an array." };
-    }
-
-    if (participantIDs.length < 2) {
-      throw { error: "createNewGroup: participantIDs should have at least 2 IDs." };
-    }
-
-    var resolveFunc = function(){};
-    var rejectFunc = function(){};
-    var returnPromise = new Promise(function (resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
+module.exports = function(http, api, ctx) {
+  function handleUpload(image) {
+    var cb;
+    var nextURL = 'https://www.facebook.com/profile/picture/upload/';
+    var rtPromise = new Promise(function (resolve, reject) {
+      cb = function (error, data) {
+        data ? resolve(data) : reject(error);
+      }
     });
 
-    if (!callback) {
-      callback = function (err, threadID) {
-        if (err) {
-          return rejectFunc(err);
-        }
-        resolveFunc(threadID);
-      };
-    }
+    if (!image) cb(null, true);
 
-    var pids = [];
-    for (var n in participantIDs) {
-      pids.push({
-        fbid: participantIDs[n]
-      });
-    }
-    pids.push({fbid: ctx.userID});
-
-    var form = {
-      fb_api_caller_class: "RelayModern",
-      fb_api_req_friendly_name: "MessengerGroupCreateMutation",
-      av: ctx.userID,
-      //This doc_id is valid as of January 11th, 2020
-      doc_id: "577041672419534",
-      variables: JSON.stringify({
-        input: {
-          entry_point: "jewel_new_group",
-          actor_id: ctx.userID,
-          participants: pids,
-          client_mutation_id: Math.round(Math.random() * 1024).toString(),
-          thread_settings: {
-            name: groupTitle,
-            joinable_mode: "PRIVATE",
-            thread_image_fbid: null
-          }
-        }
+    http
+      .postFormData(nextURL, ctx.jar, {
+        profile_id: ctx.userID,
+        photo_source: 57,
+        av: ctx.userID,
+        file: image
       })
-    };
-
-    defaultFuncs
-      .post(
-        "https://www.facebook.com/api/graphql/",
-        ctx.jar,
-        form
-      )
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.errors) {
-          throw resData;
-        }
-        return callback(null, resData.data.messenger_group_thread_create.thread.thread_key.thread_fbid);
+      .then(utils.parseAndCheckLogin(ctx, http))
+      .then(function (res) {
+        if (res.error) 
+          throw res;
+        return cb(null, res);
       })
-      .catch(function(err) {
-        log.error("createNewGroup", err);
-        return callback(err);
+      .catch(cb);
+
+    return rtPromise;
+  }
+  
+  return function createNewGroup(userIDs, title = null, image = null, callback) {
+    var cb;
+    var rtPromise = new Promise(function (resolve, reject) {
+      cb = function (error, res) {
+        res ? resolve(res) : reject(error);
+      }
+    });
+
+    if (typeof title == 'function') {
+      callback = title;
+      title = null;
+    }
+    if (!!utils.isReadableStream(title)) {
+      image = title;
+      title = null;
+    }
+    if (typeof image == 'function') {
+      callback = image;
+      image = null;
+    }
+    if (typeof callback == 'function') cb = callback;
+    if (Array.isArray(userIDs) == false) {
+      log.error('createNewGroup', 'userIDs should be an array.');
+      return cb('userIDs should be an array.');
+    }
+    if (userIDs.length < 2) {
+      log.error('createNewGroup', 'userIDs should have at least 2 IDs.');
+      return cb('userIDs should have at least 2 IDs.');
+    }
+    
+    var pids = [{ fbid: ctx.userID }];
+    for (var n in userIDs) 
+      pids.push({ fbid: userIDs[n] });
+
+    handleUpload(image)
+      .then(function (res) {
+        var form = {
+          fb_api_caller_class: "RelayModern",
+          fb_api_req_friendly_name: "MessengerGroupCreateMutation",
+          //This doc_id is valid as of January 11th, 2020
+          doc_id: "577041672419534",
+          variables: JSON.stringify({
+            input: {
+              entry_point: "jewel_new_group",
+              actor_id: ctx.userID,
+              participants: pids,
+              client_mutation_id: Math.round(Math.random() * 1024).toString(),
+              thread_settings: {
+                name: title,
+                joinable_mode: "PRIVATE",
+                thread_image_fbid: image != null ? res.payload.fbid : null
+              }
+            }
+          })
+        }
+        return http
+          .post('https://www.facebook.com/api/graphql/', ctx.jar, form)
+          .then(utils.parseAndCheckLogin(ctx, http));
+      })
+      .then(function (res) {
+        return cb(null, res.data.messenger_group_thread_create.thread.thread_key.thread_fbid);
+      })
+      .catch(function (err) {
+        log.error('createNewGroup', err);
+        return cb(err);
       });
 
-    return returnPromise;
-  };
-};
+    return rtPromise;
+  }
+}
