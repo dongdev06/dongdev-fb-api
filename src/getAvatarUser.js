431686eae2c4c7ce9bb57e66296db3d7993206d3
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-07-30 12:55:42 +0700

    Update getAvatarUser.js

--- src/getAvatarUser.js
+++ src/getAvatarUser.js
@@ -8,31 +8,34 @@ module.exports = function (http, api, ctx) {
     var cb;
     var uploads = [];
     var rtPromise = new Promise(function (resolve, reject) {
-      cb = function (error, data) {
-        data ? resolve(data) : reject(error);
-      }
+      cb = (error, data) => data ? resolve(data) : reject(error);
     });
 
-    for (let i = 0; i < userIDs.length; i++) {
+    // Getting User Data From GraphAPI In The Loop
+    userIDs.map(function (v) {
       var mainPromise = http
-        .get(`https://graph.facebook.com/${userIDs[i]}/picture?height=${height}&width=${width}&redirect=false&access_token=` + ctx.access_token, ctx.jar)
+        .get(`https://graph.facebook.com/${v}/picture?height=${height}&width=${width}&redirect=false&access_token=` + ctx.access_token, ctx.jar)
         .then(utils.parseAndCheckLogin(ctx, http))
         .then(function (res) {
-          return res.data.url;
+          return { 
+            userID: v, 
+            url: res.data.url 
+          }
         })
         .catch(function (err) {
           return cb(err);
         });
       uploads.push(mainPromise);
-    }
+    });
 
     // resolve all promises
     Promise
       .all(uploads)
       .then(function (res) {
-        var data = {};
-        for (let i = 0; i < userIDs.length; i++) data[userIDs[i]] = res[i];
-        return cb(null, data);
+        return cb(null, res.reduce(function (Obj, { userID, url }) {
+          Obj[userID] = url;
+          return Obj;
+        }, {}));
       })
       .catch(function (err) {
         return cb(err);
@@ -44,22 +47,23 @@ module.exports = function (http, api, ctx) {
   return function getAvatarUser(userIDs, size, callback) {
     var cb;
     var rtPromise = new Promise(function (resolve, reject) {
-      cb = function (err, resData) {
-        resData ? resolve(resData) : reject(err);
-      }
+      cb = (err, resData) => resData ? resolve(resData) : reject(err);
     });
 
     if (typeof size == 'function') {
       callback = size;
       size = [1500, 1500];
     }
-    if (Array.isArray(size) == false && isNaN(parseInt(size)) == false) size = [size, size];
-    if (Array.isArray(size) && size.length == 1) size = [size[0], size[0]];
+    if (Array.isArray(size) == false && utils.getType(Number(size)) == 'Number') size = [size, size];
+    else if (Array.isArray(size) && size.length == 1 && utils.getType(Number(size[0])) == 'Number') size = [size[0], size[0]];
     else size = [1500, 1500];
     if (typeof callback == 'function') cb = callback;
     if (Array.isArray(userIDs) == false) userIDs = [userIDs];
     var [height, width] = size;
-    if (ctx.access_token == 'NONE') return cb('Cant get access_token');
+    if (ctx.access_token == 'NONE') {
+      log.error('getAvatarUser', 'Cant get access_token');
+      return cb('Cant get access_token')
+    };
     
     handleAvatar(userIDs, height, width)
       .then(function (res) {
