7ad9590beca7ddd63fc13c98ea9ed6f9a69490f0
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-07-30 12:31:02 +0700

    Use Reduce To Convert Array To Object

--- src/getUserInfo.js
+++ src/getUserInfo.js
@@ -29,52 +29,47 @@ function formatData(data) {
   return retObj;
 }
 
-function formatDataGraph(data) {
-  var Obj = {};
-  for (let v in data) {
-    var { userID, res } = data[v];
-    if (res.error) Obj[userID] = {};
-    else {
-      Obj[userID] = {
-        id: userID,
-        name: res.name,
-        shortName: res.short_name || null,
-        verified: res.verified != false ? true : false,
-        email: res.email || null,
-        website: res.website || null,
-        follower: !!res.subscribers == true ? res.subscribers.summary.total_count : null,
-        lover: res.significant_other || null,
-        cover: !!res.cover == true ? res.cover.source : null,
-        first_name: res.first_name || null,
-        middle_name: res.middle_name || null,
-        last_name: res.last_name || null,
-        about: res.about || null,
-        birthday: res.birthday || null,
-        languages: res.languages || [],
-        gender: res.gender || null,
-        hometown: !!res.hometown == true ? res.hometown.name : null,
-        profileUrl: res.link || null,
-        location: !!res.location == true ? res.location.name : null,
-        username: res.username || null,
-        avatar: !!res.picture == true ? res.picture.data.url : null,
-        relationship_status: !!res.relationship_status == true ? res.relationship_status : null,
-        subscribers: !!res.subscribers == true ? res.subscribers.data : null,
-        favorite_athletes: !!res.favorite_athletes == false ? [] : res.favorite_athletes.map(function (v) {
-          return {
-            name: v.name
-          }
-        }),
-        education: !!res.education == true ? res.education.map(function(v) {
-          return {
-            type: v.type,
-            school: v.school.name
-          }
-        }) : [],
-        work: !!res.work == true ? res.work : []
-      }
+function formatDataGraph(Obj, { userID, res }) {
+  if (res.error) Obj[userID] = {};
+  else {
+    Obj[userID] = {
+      id: userID,
+      name: res.name,
+      shortName: res.short_name || null,
+      verified: res.verified != false ? true : false,
+      email: res.email || null,
+      website: res.website || null,
+      follower: !!res.subscribers == true ? res.subscribers.summary.total_count : null,
+      lover: res.significant_other || null,
+      cover: !!res.cover == true ? res.cover.source : null,
+      first_name: res.first_name || null,
+      middle_name: res.middle_name || null,
+      last_name: res.last_name || null,
+      about: res.about || null,
+      birthday: res.birthday || null,
+      languages: res.languages || [],
+      gender: res.gender || null,
+      hometown: !!res.hometown == true ? res.hometown.name : null,
+      profileUrl: res.link || null,
+      location: !!res.location == true ? res.location.name : null,
+      username: res.username || null,
+      avatar: !!res.picture == true ? res.picture.data.url : null,
+      relationship_status: !!res.relationship_status == true ? res.relationship_status : null,
+      subscribers: !!res.subscribers == true ? res.subscribers.data : null,
+      favorite_athletes: !!res.favorite_athletes == false ? [] : res.favorite_athletes.map(function (v) {
+        return {
+          name: v.name
+        }
+      }),
+      education: !!res.education == true ? res.education.map(function(v) {
+        return {
+          type: v.type,
+          school: v.school.name
+        }
+      }) : [],
+      work: !!res.work == true ? res.work : []
     }
   }
-  
   return Obj;
 }
 
@@ -132,7 +127,7 @@ module.exports = function (http, api, ctx) {
       }
       handleGetData(userIDs)
         .then(function (res) {
-          return cb(null, formatDataGraph(res));
+          return cb(null, res.reduce(formatDataGraph, {}));
         })
         .catch(function (err) {
           log.error('getUserInfo', err);
@@ -148,7 +143,7 @@ module.exports = function (http, api, ctx) {
         .then(utils.parseAndCheckLogin(ctx, http))
         .then(function(res) {
           if (res.error || res.errors) throw res;
-          return cb(null, formatData(res.payload.profiles));
+          return cb(null, formatData(res.payload.profile));
         })
         .catch(function(err) {
           log.error("getUserInfo", err);
