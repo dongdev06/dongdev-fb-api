72e5debc52334010189b2980e1d81b718e2a3316
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-08-21 20:32:07 +0700

    Fix Undefined Error When Using With "await" And Looping Feature When "By Mistake" Entering Wrong Code

--- src/getAccess.js
+++ src/getAccess.js
@@ -3,91 +3,93 @@
 var utils = require('../utils');
 var log = require('npmlog');
 
-module.exports = function (defaultFuncs, api, ctx) {
+module.exports = function (http, api, ctx) {
   return function getAccess(callback) {
     var cb;
-    var returnPromise = new Promise(function (resolve, reject) {
-      cb = function (err, res) {
-        if (err) reject(err);
-        resolve(res);
-      }
+    var url = 'https://business.facebook.com/';
+    var rt = new Promise(function (resolve, reject) {
+      cb = (error, token) => token ? resolve(token) : reject(error);
     });
 
     if (typeof callback == 'function') cb = callback;
-    if (!!ctx.access_token) return cb(null, ctx.access_token);
 
-    var nextUrl = 'https://business.facebook.com/security/twofactor/reauth/enter/';
-    var Referer = 'https://business.facebook.com/security/twofactor/reauth/?twofac_next=https%3A%2F%2Fbusiness.facebook.com%2Fbusiness_locations&type=avoid_bypass&app_id=0&save_device=0';
-    defaultFuncs
-      .get('https://business.facebook.com/business_locations', ctx.jar, null, ctx.globalOptions)
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function (html) {
-        var tokenDeprecated = /"],\["(\S+)","436761779744620",{/g.exec(html);
-        if (tokenDeprecated) {
-          ctx.access_token = tokenDeprecated[1].split('"],["').pop();
-          return cb(null, tokenDeprecated[1].split('"],["').pop());
-        }
-        var lsd = utils.getFrom(String(html), "[\"LSD\",[],{\"token\":\"", "\"}");
-        defaultFuncs
-          .post('https://business.facebook.com/security/twofactor/reauth/send/', ctx.jar, { lsd }, ctx.globalOptions, null, { Referer })
-          .then(function () {
-            var err = {
-              type: 'submitCode',
-              continue: function submitCode(code = '') {
-                if (isNaN(parseInt(code)) || code.length == 0) return cb({
-                  type: 'code-error',
-                  error: 'code is not accept'
+    var Referer = url + 'security/twofactor/reauth/?twofac_next=' + encodeURIComponent(url + 'content_management') + '&type=avoid_bypass&app_id=0&save_device=0';
+
+    if (!!ctx.access_token) cb(null, ctx.access_token);
+    else {
+      utils
+        .get(url + 'content_management', ctx.jar, null, ctx.globalOptions, null, {
+          noRef: true,
+          Origin: url
+        })
+        .then(function (res) {
+          var html = res.body;
+          var lsd = utils.getFrom(html, "[\"LSD\",[],{\"token\":\"", "\"}");
+          return lsd;
+        })
+        .then(function (lsd) {        
+          if (typeof callback != 'function') 
+            throw 'callback must be a function';
+          throw {
+            error: 'submitCode',
+            continue: function submitCode(code) {
+              var pCb;
+              var rtPromise = new Promise(function (resolve) {
+                pCb = (error, token) => resolve(cb(error, token));
+              });
+              if (typeof code != 'string')
+                return pCb({
+                  error: 'submitCode',
+                  lerror: 'code must be string',
+                  continue: submitCode
                 });
-                var form = {
+
+              http
+                .post(url + 'security/twofactor/reauth/enter/', ctx.jar, {
                   approvals_code: code,
                   save_device: true,
                   lsd 
-                };
-                defaultFuncs
-                  .post(nextUrl, ctx.jar, form, ctx.globalOptions, null, { Referer })
-                  .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-                  .then(async function (newHtml) {
-                    if (String(newHtml).includes(false)) return cb({
-                      type: 'undefined',
-                      error: 'code is not accept or something went wrong'
-                    });
-                    utils
-                      .createAccess_token(ctx.jar, ctx.globalOptions)()
-                      .then(function ([htmlData, token]) {
-                        if (token != 'NONE') {
-                          ctx.access_token = token;
-                          return cb(null, token);
-                        }
-                        return cb({
-                          type: 'token-undefined',
-                          htmlData
-                        });
-                      });
-                  })
-                  .catch(function (err) {
-                    log.error('getAccess', err);
-                    return cb({
-                      type: 'error',
-                      error: err
-                    });
-                  });
-              }
-            };
-            if (typeof callback == 'function') return cb(err);
-            return cb({
-              type: 'callback',
-              message: 'callback is not a function'
-            });
-          });
-      })
-      .catch(function (err) {
-        log.error('getAccess', err);
-        return cb({
-          type: 'error',
-          error: err
-        });
-      })
+                }, ctx.globalOptions, null, {
+                  Referer,
+                  Origin: url
+                })
+                .then(function (res) {
+                  var { payload } = JSON.parse(res.body.split(';').pop() || '{}');
+                  if (payload && !payload.codeConfirmed)
+                    throw {
+                      error: 'submitCode',
+                      lerror: payload.message,
+                      continue: submitCode
+                    }
+                  
+                  return res;
+                })
+                .then(utils.getAccessFromBusiness(ctx.jar, ctx.globalOptions))
+                .then(function (res) {
+                  var [html, token] = res;
+                  if (!token) 
+                    throw {
+                      error: 'token-undefined',
+                      htmlData: html
+                    }
+                  ctx.access_token = token;                      
+                  return pCb(null, token);
+                })
+                .catch(function (res) {
+                  log.error('getAccess', res.error || res);
+                  return pCb(res);
+                });
 
-    return returnPromise;
+              return rtPromise;
+            }
+          }        
+        })
+        .catch(function (res) {
+          log.error('getAccess', res.error || res);
+          return cb(res);
+        });
+    }
+
+    return rt;
   }
 }
