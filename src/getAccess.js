d9e5fb747574c47b9cb9a41ee5fe3153e115b31e
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-09-01 12:51:36 +0700

    Add the authentication code input so that when using "await" you can still get the token

--- src/getAccess.js
+++ src/getAccess.js
@@ -4,19 +4,22 @@ var utils = require('../utils');
 var log = require('npmlog');
 
 module.exports = function (http, api, ctx) {
-  return function getAccess(callback) {
+  return function getAccess(authCode = '', callback) {
     var cb;
     var url = 'https://business.facebook.com/';
+    var Referer = url + 'security/twofactor/reauth/?twofac_next=' + encodeURIComponent(url + 'content_management') + '&type=avoid_bypass&app_id=0&save_device=0';
     var rt = new Promise(function (resolve, reject) {
       cb = (error, token) => token ? resolve(token) : reject(error);
     });
 
+    if (typeof authCode == 'function') {
+      callback = authCode;
+      authCode = '';
+    }
     if (typeof callback == 'function') cb = callback;
-
-    var Referer = url + 'security/twofactor/reauth/?twofac_next=' + encodeURIComponent(url + 'content_management') + '&type=avoid_bypass&app_id=0&save_device=0';
-
-    if (!!ctx.access_token) cb(null, ctx.access_token);
-    else {
+    if (!!ctx.access_token) 
+      cb(null, ctx.access_token);
+    else 
       utils
         .get(url + 'content_management', ctx.jar, null, ctx.globalOptions, null, {
           noRef: true,
@@ -27,23 +30,19 @@ module.exports = function (http, api, ctx) {
           var lsd = utils.getFrom(html, "[\"LSD\",[],{\"token\":\"", "\"}");
           return lsd;
         })
-        .then(function (lsd) {        
-          if (typeof callback != 'function') 
-            throw 'callback must be a function';
-          throw {
-            error: 'submitCode',
-            continue: function submitCode(code) {
-              var pCb;
-              var rtPromise = new Promise(function (resolve) {
-                pCb = (error, token) => resolve(cb(error, token));
+        .then(function (lsd) {
+          function submitCode(code) {
+            var pCb;
+            var rtPromise = new Promise(function (resolve) {
+              pCb = (error, token) => resolve(cb(error, token));
+            });
+            if (typeof code != 'string')
+              pCb({
+                error: 'submitCode',
+                lerror: 'code must be string',
+                continue: submitCode
               });
-              if (typeof code != 'string')
-                return pCb({
-                  error: 'submitCode',
-                  lerror: 'code must be string',
-                  continue: submitCode
-                });
-
+            else 
               http
                 .post(url + 'security/twofactor/reauth/enter/', ctx.jar, {
                   approvals_code: code,
@@ -62,33 +61,51 @@ module.exports = function (http, api, ctx) {
                       continue: submitCode
                     }
                   
-                  return res;
+                  return;
                 })
-                .then(utils.getAccessFromBusiness(ctx.jar, ctx.globalOptions))
+                .then(function () {
+                  return utils
+                    .get(url + 'content_management', ctx.jar, null, ctx.globalOptions, null, { noRef: true })
+                    .then(function (res) {
+                      var html = res.body;
+                      var token = /"accessToken":"(\S+)","clientID":/g.exec(html);
+
+                      return [html, token];
+                    });
+                })
                 .then(function (res) {
-                  var [html, token] = res;
-                  if (!token) 
+                  if (!res[1]) 
                     throw {
                       error: 'token-undefined',
-                      htmlData: html
+                      htmlData: res[0]
                     }
-                  ctx.access_token = token;                      
-                  return pCb(null, token);
+                  ctx.access_token = res[1][1];                      
+                  return pCb(null, res[1][1]);
                 })
-                .catch(function (res) {
-                  log.error('getAccess', res.error || res);
-                  return pCb(res);
+                .catch(function (err) {
+                  log.error('getAccess', err.error || err);
+                  return pCb(err);
                 });
+    
+            return rtPromise;
+          }
 
-              return rtPromise;
+          if (authCode.length == 6 && !isNaN(authCode)) 
+            submitCode(authCode.toString());
+          else if (typeof callback == 'function') 
+            throw {
+              error: 'submitCode',
+              continue: submitCode
             }
-          }        
+          else 
+            throw {
+              error: 'authentication code must be string or number or callback must be a function to continue'
+            }
         })
-        .catch(function (res) {
-          log.error('getAccess', res.error || res);
-          return cb(res);
+        .catch(function (err) {
+          log.error('getAccess', typeof callback == 'function' ? (err.error || err) : err);
+          return cb(err);
         });
-    }
 
     return rt;
   }
