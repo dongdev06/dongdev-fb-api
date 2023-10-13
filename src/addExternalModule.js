cedbe265b653b33c8a2de06ef7d7b2ca5abd8f23
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-10-13 22:56:37 +0700

    Update addExternalModule.js

--- src/addExternalModule.js
+++ src/addExternalModule.js
@@ -1,19 +1,17 @@
-"use strict";
+'use strict';
 
-const utils = require("../utils");
+var utils = require('../utils');
 
-module.exports = function(defaultFuncs, api, ctx) {
+module.exports = function (http, api, ctx) {
   return function addExternalModule(moduleObj) {
-    if (utils.getType(moduleObj) == "Object") {
-        for (let apiName in moduleObj) {
-            if (utils.getType(moduleObj[apiName]) == "Function") {
-                api[apiName] = moduleObj[apiName](defaultFuncs, api, ctx);
-            } else {
-                throw new Error(`Item "${apiName}" in moduleObj must be a function, not ${utils.getType(moduleObj[apiName])}!`);
-            }
-        }
-    } else {
-        throw new Error(`moduleObj must be an object, not ${utils.getType(moduleObj)}!`);
-    }
-  };
-};
+    if (utils.getType(moduleObj) == 'Object') {
+      for (let name in moduleObj) {
+        if (utils.getType(moduleObj[name]) == 'Function') 
+          api[name] = moduleObj[name](http, api, ctx);
+        else 
+          throw new Error(`Item "${name}" in moduleObj must be a function, not ${utils.getType(moduleObj[name])}!`);
+      }
+    } else
+      throw new Error(`moduleObj must be an object, not ` + utils.getType(moduleObj) + `!`);
+  }
+}
