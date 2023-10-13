7db78a2af7d9575137434f90629eaf2779406747
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-10-13 13:26:51 +0700

    Clean Code

--- src/changeCover.js
+++ src/changeCover.js
@@ -1,55 +1,39 @@
-"use strict";
+'use strict';
 
 var utils = require('../utils.js');
-var log = require("npmlog");
+var log = require('npmlog');
 
 module.exports = function (http, api, ctx) {
-  function handleUpload(image) {
-    var cb;
-    var nextURL = 'https://www.facebook.com/profile/picture/upload/';
-    var rtPromise = new Promise(function (resolve, reject) {
-      cb = function (error, data) {
-        data ? resolve(data) : reject(error);
-      }
-    });
-
-    if (!utils.isReadableStream(image)) 
-      cb('image is not a readable stream');
-
-    http
-      .postFormData(nextURL, ctx.jar, {
-        profile_id: ctx.userID,
-        photo_source: 57,
-        av: ctx.userID,
-        file: image
-      })
-      .then(utils.parseAndCheckLogin(ctx, http))
-      .then(function (res) {
-        if (res.error) 
-          throw res;
-        return cb(null, res);
-      })
-      .catch(cb);
-
-    return rtPromise;
-  }
-
   return function changeCover(image, callback) {
     var cb;
-    var rtPromise = new Promise(function (resolve, reject) {
-      cb = function (error, data) {
-        data ? resolve(data) : reject(error);
-      }
+    var rt = new Promise(function (resolve, reject) {
+      cb = (error, url) => error ? reject(error) : resolve(url);
     });
-    
-    if (typeof callback == 'function') cb = callback;
 
-    handleUpload(image)
-      .then(function (res) {
-        var form = {
-          fb_api_caller_class: "RelayModern",
-          fb_api_req_friendly_name: "ProfileCometCoverPhotoUpdateMutation",
-          variables: JSON.stringify({
+    if (typeof image == 'function') {
+      callback = image;
+      image = null;
+    }
+    if (typeof callback == 'function') cb = callback;
+    if (!utils.isReadableStream(image)) {
+      var error = 'image should be a readable stream, not ' + utils.getType(image);
+      log.error('changeCover', error);
+      cb(error);
+    }
+    else {
+      http
+        .postFormData('https://www.facebook.com/profile/picture/upload/', ctx.jar, {
+          profile_id: ctx.userID,
+          photo_source: 57,
+          av: ctx.userID,
+          file: image
+        })
+        .then(utils.parseAndCheckLogin(ctx, http))
+        .then(function (res) {
+          if (res.error || res.errors || !res.payload) 
+            throw res;
+
+          var vari = {
             input: {
               attribution_id_v2: `ProfileCometCollectionRoot.react,comet.profile.collection.photos_by,unexpected,${Date.now()},770083,,;ProfileCometCollectionRoot.react,comet.profile.collection.photos_albums,unexpected,${Date.now()},470774,,;ProfileCometCollectionRoot.react,comet.profile.collection.photos,unexpected,${Date.now()},94740,,;ProfileCometCollectionRoot.react,comet.profile.collection.saved_reels_on_profile,unexpected,${Date.now()},89669,,;ProfileCometCollectionRoot.react,comet.profile.collection.reels_tab,unexpected,${Date.now()},152201,,`,
               cover_photo_id: res.payload.fbid,
@@ -58,30 +42,32 @@ module.exports = function (http, api, ctx) {
                 y: 1
               },
               target_user_id: ctx.userID,
-    actor_id: ctx.userID, 
+              actor_id: ctx.userID, 
               client_mutation_id: Math.round(Math.random() * 19).toString()
             },
             scale: 1,
             contextualProfileContext: null
-          }),
-          server_timestamps: !0,
-          doc_id: "8247793861913071"
-        }
-        return http
-          .post('https://www.facebook.com/api/graphql', ctx.jar, form)
-          .then(utils.parseAndCheckLogin(ctx, http));
-      })
-      .then(function (res) {
-        if (res.errors) throw res;
-        return cb(null, {
-          url: res.data.user_update_cover_photo.user.cover_photo.photo.url
+          }
+          return http
+            .post('https://www.facebook.com/api/graphql', ctx.jar, {
+              doc_id: 8247793861913071,
+              server_timestamps: true,
+              fb_api_req_friendly_name: 'ProfileCometCoverPhotoUpdateMutation',
+              variables: JSON.stringify(vari)
+            })
+            .then(utils.parseAndCheckLogin(ctx, http));
+        })
+        .then(function (res) {
+          if (res.errors) 
+            throw res;
+          return cb(null, res.data.user_update_cover_photo.user.cover_photo.photo.url);
+        })
+        .catch(function (err) {
+          log.error('changeCover', err);
+          return cb(err);
         });
-      })
-      .catch(function (err) {
-        log.error('changeCover', err);
-        return cb(err);
-      });
-    
-    return rtPromise;
+    }
+
+    return rt;
   }
 }
