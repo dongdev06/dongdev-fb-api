08001a01d020ad9dd41480fb558895666851292a
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-11-14 12:09:58 +0700

    ReBuild

--- src/httpGet.js
+++ src/httpGet.js
@@ -1,53 +1,53 @@
-"use strict";
+'use strict';
 
-var utils = require("../utils");
-var log = require("npmlog");
+var utils = require('../utils.js');
+var log = require('npmlog');
 
-module.exports = function (defaultFuncs, api, ctx) {
-  return function httpGet(url, form, customHeader, callback, notAPI) {
+module.exports = function (http, api, ctx) {
+  return function httpGet(url, qs, customHeader, callback, notAPI) {
     var cb;
     var returnPromise = new Promise(function (resolve, reject) {
-      cb = function (err, resData) {
-        if (err) reject(err);
-        resolve(resData);
-      }
+      cb = (error, body) => body ? resolve(body) : reject(error);
     });
 
-    if (typeof form == 'function') {
-      callback = form;
-      form = null;
+    if (typeof qs == 'function') {
+      callback = qs;
+      qs = {}
+    }
+    if (typeof qs == 'boolean') {
+      notAPI = qs;
+      qs = {}
     }
     if (typeof customHeader == 'function') {
       callback = customHeader;
+      customHeader = {}
+    }
+    if (typeof customHeader == 'boolean') {
+      notAPI = customheader;
       customHeader = {}
     }
     if (typeof callback == 'boolean') {
       notAPI = callback;
       callback = null;
     }
-    if (typeof callback == 'function') cb = callback;
+    if (typeof callback == 'function') 
+      cb = callback;
 
-    if (notAPI) {
-      utils
-        .get(url, ctx.jar, form, ctx.globalOptions, ctx, customHeader)
-        .then(function (resData) {
-          return cb(null, String(resData.body));
-        })
-        .catch(function (err) {
-          log.error('httpGet', err);
-          return cb(err);
-        });
-    } else {
-      defaultFuncs
-        .get(url, ctx.jar, form, {}, ctx, customHeader)
-        .then(function (resData) {
-          return cb(null, String(resData.body));
-        })
-        .catch(function (err) {
-          log.error('httpGet', err);
-          return cb(err);
-        });
-    }
+    let mainPromise;
+    if (notAPI) 
+      mainPromise = utils.get(url, ctx.jar, qs, ctx.globalOptions, ctx, customHeader);
+    else 
+      mainPromise = http.get(url, ctx.jar, qs, null, null, customHeader);
+
+    mainPromise
+      .then(function (res) {
+        var body = res.body.toString();
+        return cb(null, body);
+      })
+      .catch(function (err) {
+        log.error('httpGet', err);
+        return cb(err);
+      });
 
     return returnPromise;
   }
