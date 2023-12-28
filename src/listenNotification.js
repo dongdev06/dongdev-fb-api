2948f462cabb4fae2347658d5af301103f8de67b
Gia Khang (Sam) <100568006+KhangGia1810@users.noreply.github.com>
2023-12-28 13:58:16 +0700

    Update listenNotification.js

--- src/listenNotification.js
+++ src/listenNotification.js
@@ -2,87 +2,84 @@
 
 var utils = require('../utils.js');
 var log = require('npmlog');
+var EventEmitter = require('node:events');
 
-function formatGraphResponse(data) {
-  var Obj = [];
-  var convertMS = function (ms, date) {
-    var time = (date - ms) / 1000;
-    return time;
-  };
-  var edges = data.viewer.notifications_page.edges;
-
-  for (let res of edges) {
-    res = res.node;
-    if (res.row_type !== 'NOTIFICATION') continue;
-    var timestamp = res.notif.creation_time.timestamp * 1000;
-    if (convertMS(timestamp, Date.now()) > 60) break;
-    var data = {
-      id: res.notif.id,
-      noti_id: res.notif.notif_id,
-      type: res.notif.notif_type,
-      body: res.notif.body,
-      url: res.notif.url,
-      timestamp
-    }
-    Obj.push(data);
+function format(res, globalCallback) {
+  var checkMinutes = (date_1, date_2) => {
+    let ms_1 = date_1.getTime();
+    let ms_2 = date_2.getTime();
+    return Math.ceil((ms_2 - ms_1) / (60 * 1000));
   }
-  
-  return Obj;
+
+  for (let index of res.notifications_page.edges) {
+    if (index.node.row_type !== 'NOTIFICATION') continue;
+
+    var timestamp = index.node.notif.creation_time.timestamp;
+    if (checkMinutes(new Date(timestamp * 1000), new Date()) <= 1) 
+      globalCallback(null, {
+        id: res.node.notif.id,
+        noti_id: res.node.notif.notif_id,
+        body: index.node.notif.body.text,
+        url: index.node.notif.url,
+        timestamp: timestamp * 1000
+      });
+  }
 }
 
 module.exports = function (http, api, ctx) {
-  function listen(callback) {
-    var interval = setInterval(function (form) {
+  let globalCallback, interval;
+
+  function MessageRepeat() {
+    interval = setInterval(function () {
       return http
-        .post('https://www.facebook.com/api/graphql/', ctx.jar, form)
+        .post('https://www.facebook.com/api/graphql/', ctx.jar, {
+          fb_api_req_friendly_name: 'CometNotificationsRootQuery',
+          doc_id: 6663491207045267,
+          variables: JSON.stringify({
+            count: 5,
+            environment: 'MAIN_SURFACE',
+            filter_tokens: ['Cg8CZnQPA2FsbAE='],
+            scale: 1
+          }),
+          server_timestamps: !0
+        })
         .then(utils.parseAndCheckLogin(ctx, http))
         .then(function (res) {
-          if (res.error) throw res;
-          return http
-            .post('https://www.facebook.com/ajax/navigation/', ctx.jar, {
-              client_previous_actor_id: '',
-              route_url: '/notifications',
-              routing_namespace: 'fb_comet'
-            })
-            .then(utils.parseAndCheckLogin(ctx, http))
-            .then(function () {
-              return callback(null, formatGraphResponse(res.data));
-            });
+          if (res.error || res.errors) 
+            throw res;
+
+          return format(res.data.viewer, globalCallback);
         })
         .catch(function (err) {
           log.error('listenNotification', err);
-          return callback(err);
+          clearInterval(interval);
+          interval = void 0;
+          return globalCallback(err);
         });
-    }, 60000, {
-      fb_api_req_friendly_name: "CometNotificationsRootQuery",
-      doc_id: "6663491207045267",
-      variables: JSON.stringify({
-        count: 15,
-        environment: "MAIN_SURFACE",
-        filter_tokens: ["Cg8CZnQPA2FsbAE="],
-        scale: 1
-      }),
-      server_timestamps: !0
-    });
-
-    return function stopListen() {
-      return clearInterval(interval);
-    }
+    }, 60 * 1000);
   }
-  
-  return function GraphNoti(callback) {
-    if (typeof callback != 'function') {
-      var error = new Error('callback is not a function');
-      log.error('listenNotification', error);
-      return error;
+
+  return function notification(callback) {
+    class MessageEmitter extends EventEmitter {
+      stop() {
+        globalCallback = () => {}
+
+        if (interval) {
+          clearInterval(interval);
+          interval = void 0;
+        }
+        Message.emit('stop', new Date());
+      }
     }
 
-    try {
-      var rCb = listen(callback);
-      return rCb;
-    } catch (err) {
-      log.error('listenNotification', err);
-      return err;
-    }
+    var Message = new MessageEmitter();
+
+    if (typeof callback == 'function')
+      globalCallback = callback;
+    else 
+      globalCallback = (error, message) => error ? Message.emit('error', error) : Message.emit('message', message);
+
+    MessageRepeat();
+    return Message;
   }
 }
